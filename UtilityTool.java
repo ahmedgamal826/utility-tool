6db36dc041522a94e5e224a53e88ffec74c48b80
Ahmed Gamal Elnemr <ahmedgamal826@gmail.com>
2025-11-16 15:49:19 +0200

    Add max number feature

--- UtilityTool.java
+++ UtilityTool.java
@@ -3,12 +3,18 @@ public class UtilityTool {
     public static void main(String[] args) {
         System.out.println("Welcome to My Utility Tool!");
         System.out.println("Sum Result = " + addNumbers(1, 2));
+        System.out.println("Max Number = " + findMax(10, 7));
+
         System.out.println("Current Time: " + getCurrentTime());
         System.out.println("Hostname: " + getHostName());
     }
 
     public static int addNumbers(int a, int b) {
         return a + b;
+    }
+
+    public static int findMax(int a, int b) {
+    return (a > b) ? a : b;
     }
 
     public static String getCurrentTime() {
